cc650a5c1fdbf5f469587cbe735654aa8de174a5
Mikhail Petrov <hsestudent21@yandex.ru>
2020-07-04 17:30:49 +0300

    v stepik 1.0

--- ds_collecting.py
+++ ds_collecting.py
@@ -1,10 +1,17 @@
 import requests
 import re
 
-OPENEDU_URL = "https://openedu.ru/course/"
+STEPIK_URL = "https://stepik.org/course/{}/promo"
 
-response = requests.get(OPENEDU_URL)
+with open('course_list_html.txt', 'w') as f:
+    for i in range(2000):
+        response = requests.get(STEPIK_URL.format(str(i)))
+        if (response.status_code != 404):
+            f.write(str(i)+'\n')
+        else:
+            print('E: page {} does not exists'.format(STEPIK_URL.format(str(i))))
 
+"""
 hrefs = re.findall('["\']https[A-Za-z0-9:/.]+["\']', response.text)
 
 with open('hrefs_list.txt', 'w') as f:
@@ -14,3 +21,6 @@ with open('hrefs_list.txt', 'w') as f:
 
 with open('course_list_html.txt', 'w') as f:
     f.write(response.text)
+
+requests.models.Response
+"""
