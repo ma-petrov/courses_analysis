0bd3c6163c50a4802837ae9064e3043d48282e41
Mikhail Petrov <hsestudent21@yandex.ru>
2020-07-04 13:48:24 +0300

    patch

--- ds_collecting.py
+++ ds_collecting.py
@@ -1,11 +1,16 @@
 import requests
+import re
 
 OPENEDU_URL = "https://openedu.ru/course/"
 
 response = requests.get(OPENEDU_URL)
 
+hrefs = re.findall('href="[A-Za-z0-9:/.]+"', response.text)
+
+with open('hrefs_list.txt', 'w') as f:
+    for href in hrefs:
+        f.write(href)
+        f.write('\n')
+
 with open('course_list_html.txt', 'w') as f:
-    if (f.write(response.text)):
-        print('log (i): html is writed to file')
-    else:
-        print('log (e): writing html to file error')
+    f.write(response.text)
