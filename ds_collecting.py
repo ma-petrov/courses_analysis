0938e794063d524a1fa015c1ff26bfc8d86e6ab2
Mikhail Petrov <hsestudent21@yandex.ru>
2020-07-08 21:30:44 +0300

    initial commit

--- ds_collecting.py
+++ ds_collecting.py
@@ -1,22 +1,34 @@
-"""
-Version 2.0 (Stepik)
-Site parcing based on Selenium WebDriver
-"""
-
 import requests
 import re
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import lxml.html as html
+from bs4 import BeautifulSoup
 
 STEPIK_URL = "https://stepik.org/course/{}/promo"
 
-"""
-driver = webdriver.Chrome(executable_path='C:/ProgramData/chromedriver_win32/chromedriver.exe')
-driver.get(STEPIK_URL.format('1'))
-driver.close()
-"""
+def parse_course_page(page_num): #71439
+    soup = BeautifulSoup(requests.get(STEPIK_URL.format(page_num)).text, 'lxml')
+    """
+    with open('course_page_content.txt', 'w') as f:
+        for element in soup.find_all('div'):
+            f.writelines(element.lines)
+    """
+    for tag in soup.find_all(class_='toc-promo__lesson toc-promo-lesson ember-view'):
+        print(tag)
+        """
+        regex = re.compile('[А-Яа-я]+')
+        text = regex.findall(tag.name)
+        print(text)
+        """
 
-driver = webdriver.Chrome(executable_path='C:/ProgramData/chromedriver_win32/chromedriver.exe')
-driver.get(STEPIK_URL.format(71439))
-element = driver.find_element_by_class_name('toc-promo-lesson__title')
-print(element)
+        
+
+# Content description class: 'toc-promo-lesson__title'
+# 'toc-promo__lesson toc-promo-lesson ember-view'
+
+parse_course_page(71439)
+
+
+
+"""
+hrefs = re.findall('["\']https[A-Za-z0-9:/.]+["\']', response.text)
+"""
