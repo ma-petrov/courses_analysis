ac06c0c5c209bea5e3586868493546ad627373b8
Mikhail Petrov <hsestudent21@yandex.ru>
2020-07-08 21:51:29 +0300

    patch

--- ds_collecting.py
+++ ds_collecting.py
@@ -3,31 +3,28 @@ import re
 import lxml.html as html
 from bs4 import BeautifulSoup
 
-STEPIK_URL = "https://stepik.org/course/{}/promo"
+ONLINE_EDU_URL = 'https://online.edu.ru/public/course?faces-redirect=true&cid=3476'
 
-def parse_course_page(page_num): #71439
-    soup = BeautifulSoup(requests.get(STEPIK_URL.format(page_num)).text, 'lxml')
-    """
-    with open('course_page_content.txt', 'w') as f:
-        for element in soup.find_all('div'):
-            f.writelines(element.lines)
-    """
-    for tag in soup.find_all(class_='toc-promo__lesson toc-promo-lesson ember-view'):
-        print(tag)
-        """
-        regex = re.compile('[А-Яа-я]+')
-        text = regex.findall(tag.name)
+def parse_course_page(url):
+    soup = BeautifulSoup(requests.get(url).text, 'lxml')
+    
+    for tag in soup.find_all(class_='ui-outputpanel ui-widget user-profile-view'):
+        regex = re.compile(r'[0-9]+.\s[А-Яа-я0-9\-\s,.]+')
+        text = regex.findall(str(tag))
+        #print(text)
+
+    for tag in soup.find_all(class_='ui-outputpanel ui-widget'):
+        regex = re.compile(r'[0-9]+.\s[А-Яа-я0-9\-\s,.]+')
+        text = regex.findall(str(tag))
         print(text)
-        """
-
-        
-
-# Content description class: 'toc-promo-lesson__title'
-# 'toc-promo__lesson toc-promo-lesson ember-view'
-
-parse_course_page(71439)
 
 
+
+parse_course_page(ONLINE_EDU_URL)
+
+
+# 'ui-outputpanel ui-widget user-profile-view'
+# 'ui-outputpanel ui-widget
 
 """
 hrefs = re.findall('["\']https[A-Za-z0-9:/.]+["\']', response.text)
