9d974b3b95a00865cb754b040ee1c6fa7b025577
Mikhail Petrov <hsestudent21@yandex.ru>
2020-07-08 20:09:18 +0300

    patch

--- ds_collecting.py
+++ ds_collecting.py
@@ -10,6 +10,13 @@ from selenium.webdriver.common.keys import Keys
 
 STEPIK_URL = "https://stepik.org/course/{}/promo"
 
+"""
 driver = webdriver.Chrome(executable_path='C:/ProgramData/chromedriver_win32/chromedriver.exe')
 driver.get(STEPIK_URL.format('1'))
 driver.close()
+"""
+
+driver = webdriver.Chrome(executable_path='C:/ProgramData/chromedriver_win32/chromedriver.exe')
+driver.get(STEPIK_URL.format(71439))
+element = driver.find_element_by_class_name('toc-promo-lesson__title')
+print(element)
