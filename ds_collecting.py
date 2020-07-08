0307ebb9d196f9bd6de6602bf91afe8e77d5bd53
Mikhail Petrov <hsestudent21@yandex.ru>
2020-07-08 20:15:53 +0300

    add get_page_html function

--- ds_collecting.py
+++ ds_collecting.py
@@ -3,14 +3,21 @@ import re
 
 STEPIK_URL = "https://stepik.org/course/{}/promo"
 
+def get_page_html(url):
+    response = requests.get(url)
+    if (response.status_code != 404):
+        return response.text
+    else:
+        print('E: page {} does not exists'.format(url))
+        return ''
+"""
 with open('course_list_html.txt', 'w') as f:
     for i in range(2000):
-        response = requests.get(STEPIK_URL.format(str(i)))
-        if (response.status_code != 404):
-            f.write(str(i)+'\n')
-        else:
-            print('E: page {} does not exists'.format(STEPIK_URL.format(str(i))))
+        f.write(get_page_html(STEPIK_URL.format(str(i))))
+"""      
 
+with open('course_list_html.txt', 'w') as f:
+    get_page_html(STEPIK_URL.format(71439))
 """
 hrefs = re.findall('["\']https[A-Za-z0-9:/.]+["\']', response.text)
 
