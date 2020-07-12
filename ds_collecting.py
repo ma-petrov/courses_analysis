34427d65b44432ecbac9270b5e89f765aa487446
Mikhail Petrov <hsestudent21@yandex.ru>
2020-07-13 00:38:17 +0300

    version 1.0.0

--- ds_collecting.py
+++ ds_collecting.py
@@ -13,7 +13,7 @@ COURSERA_URL = 'https://ru.coursera.org'
 CATALOG_PAGE = '/courses?page={}&index=prod_all_products_term_optimization'
 
 # dataset variables:
-NAME = 'COURSE_NAME'
+TITLE = 'TITLE'
 RATING = 'RATING'
 VAR_1 = 'VAR_1'
 VAR_2 = 'VAR_2'
@@ -33,12 +33,11 @@ def main():
 
 def parse_specialization_page(url):
     """
-    Returns list of dictionries each of contains url and mark of course
+    Returns list of course page urls
     Params:
     url - url of specialization page
     """
     try:
-        #soup = BeautifulSoup(requests.get(COURSERA_URL + url).text, 'lxml')
         response = requests.get(COURSERA_URL + url)
         response.encoding = 'utf-8'
         soup = BeautifulSoup(response.text, 'lxml')
@@ -48,9 +47,7 @@ def parse_specialization_page(url):
 
     data = []
     for tag in soup.find_all(class_=re.compile('CourseItem')):
-        url = tag.find('a').get('href')
-        rating = tag.find(class_=re.compile('rating-text')).next_element
-        data.append({'course_url': url, RATING: rating})
+        data.append(tag.find('a').get('href'))
 
     return data
 
@@ -66,7 +63,6 @@ def parse_course_page(url):
     PARAM_1 = 'начал новую карьеру, пройдя эти курсы'
     PARAM_2 = 'получил значимые преимущества в карьере благодаря этому курсу'
     PARAM_3 = 'стал больше зарабатывать или получил повышение'
-
     param_dict = {PARAM_1: VAR_1, PARAM_2: VAR_2, PARAM_3: VAR_3}
 
     try:
@@ -77,15 +73,25 @@ def parse_course_page(url):
         print('Error: course page {} not found'.format(url))
         return None
 
-    data = {PARAM_1: None, PARAM_2: None, PARAM_3: None}
+    data = {TITLE: '', RATING: None, VAR_1: None, VAR_2: None, VAR_3: None}
+    
+    data.update({TITLE: soup.find('h1', {'class': 'banner-title m-b-0 banner-title-without--subtitle'}).next_element})
+
+    try:
+        rating = 'empty-rating'
+        rating = soup.find('span', {'class': '_16ni8zai m-b-0 rating-text number-rating number-rating-expertise'}).next_element
+        data.update({RATING: float(rating)})
+    except:
+        print('Error: cant convert value {} to float'.format(rating))
+
     for tag in soup.find_all('div', class_='_1k3yl1y'):
         param = tag.find('div', class_='font-sm').next_element
         value = tag.find('h2').next_element
         try:
             if param in param_dict.keys():
-                data.update({param_dict.get(param): int(value)})
+                data.update({param_dict.get(param): float(value)})
         except:
-            print('Error: cant convert value {} to int'.format(value))
+            print('Error: cant convert value {} to float'.format(value))
 
     return data
 
@@ -99,6 +105,7 @@ def main():
 
     # get first page of course catalog
     for i in range(1):
+        # Gettig soup object of courses catalog page
         try:
             response = requests.get(COURSERA_URL + CATALOG_PAGE.format(i+1))
             response.encoding = 'utf-8'
@@ -107,26 +114,15 @@ def main():
             print('Error: page {} dont reply'.format(CATALOG_PAGE.format(i+1)))
             return
         
+        # Collecting tags with links to courses of specializations pages and calling parsing functions for them
         for tag in soup.find_all('li', class_='ais-InfiniteHits-item'):
             url = tag.find('a').get('href')
             card_info = tag.find(class_='card-info vertical-box')
             page_type = card_info.find(class_='_jen3vs _1d8rgfy3').next_element
 
-            data = {NAME: '', RATING: None, VAR_1: None, VAR_2: None, VAR_3: None}
+            data = {TITLE: '', RATING: None, VAR_1: None, VAR_2: None, VAR_3: None}
             
             if page_type == 'Курс':
-                # Card links to course page: collecting course name, rating and calling course page parsing function
-                # Collecting rating:
-                rating = tag.find('span', class_='ratings-text').next_element
-                try:
-                    data.update({RATING: int(rating)})
-                except:
-                    print('Error: cant convert {} to int'.format(rating))
-                
-                # Collecting course name:
-                name = card_info.find('h2', class_='color-primary-text card-title headline-1-text').next_element
-                data.update({NAME: name})
-
                 # Calling course page parsing function by link in tag 'a':
                 # If parse_course_page is unseccessful it will return None
                 # If successful - data would updated with course vars and would inserted into sample
@@ -136,21 +132,24 @@ def main():
                     sample.append(data)
 
             elif page_type == 'Специализация':
+                # Calling specialization page parsing function and collecting corse links
+                # Then calling course page parsing function by links in courses list
                 courses = parse_specialization_page(url)
                 if courses != None:
                     for course in courses:
-                        course_vars = parse_course_page(course.get('course_url'))
+                        course_vars = parse_course_page(course)
                         if course_vars != None: 
-                            data.update({RATING: course.get(RATING)})
                             data.update(course_vars)
                             sample.append(data)
 
             else:
+                # Not usable, only pass
                 pass
 
     df = pd.DataFrame(sample)
     df.to_csv('sample.csv')
             
+
 
 if __name__ == "__main__":
     main()
