10c5dab4929a3d67b47620f1fdea86e339c267fc
Mikhail Petrov <hsestudent21@yandex.ru>
2020-07-12 15:22:49 +0300

    click to course dont work

--- ds_collecting.py
+++ ds_collecting.py
@@ -1,5 +1,5 @@
 """
-Version 2.0 (Stepik)
+Version 2.0 (Coursera)
 Site parcing based on Selenium WebDriver
 """
 
@@ -7,16 +7,38 @@ import requests
 import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 
-STEPIK_URL = "https://stepik.org/course/{}/promo"
+COURSERA_URL = 'https://ru.coursera.org/courses'
 
-"""
-driver = webdriver.Chrome(executable_path='C:/ProgramData/chromedriver_win32/chromedriver.exe')
-driver.get(STEPIK_URL.format('1'))
-driver.close()
-"""
+# function make driver waits 20 seconds and find elemet by class name
+def wait_and_find_by_class(driver, element):
+    try:
+        return WebDriverWait(driver, 20).until(lambda driver : driver.find_element_by_class_name(element))
+    finally:
+        pass
 
-driver = webdriver.Chrome(executable_path='C:/ProgramData/chromedriver_win32/chromedriver.exe')
-driver.get(STEPIK_URL.format(71439))
-element = driver.find_element_by_class_name('toc-promo-lesson__title')
-print(element)
+# 
+def main():
+
+    course_mark = []
+
+    driver = webdriver.Chrome(executable_path='C:/ProgramData/chromedriver_win32/chromedriver.exe')
+    driver.get(COURSERA_URL)
+    driver.implicitly_wait(20)
+
+    # finding element with course mark and adding in to list course_mark
+    element = driver.find_element_by_class_name('ratings-text')
+    course_mark.append(str(element))
+    print(course_mark)
+
+    #element = wait_and_find_by_class(driver, 'ratings-text')
+    #course_mark.append(str(element))
+    #print(course_mark)
+
+    element = driver.find_element_by_class_name('image-wrapper vertical-box')
+    element.click()
+
+if __name__ == "__main__":
+    main()
+
