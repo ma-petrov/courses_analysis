2ff9a2e8bb02e78a212a617846186c95374cba51
Mikhail Petrov <hsestudent21@yandex.ru>
2020-07-14 21:18:47 +0300

    added new vars

--- ds_collecting.py
+++ ds_collecting.py
@@ -1,6 +1,8 @@
 """
-Version 1.0 (Coursera)
+Version 1.1 (Coursera)
 Site parcing by BeautifulSoup
+
+Added parameters to dataset
 """
 
 import pandas as pd
@@ -8,28 +10,8 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-
 COURSERA_URL = 'https://ru.coursera.org'
 CATALOG_PAGE = '/courses?page={}&index=prod_all_products_term_optimization'
-
-# dataset variables:
-TITLE = 'TITLE'
-RATING = 'RATING'
-VAR_1 = 'VAR_1'
-VAR_2 = 'VAR_2'
-VAR_3 = 'VAR_3'
-
-# is used for testing
-def get_html_and_save_to_file(url):
-    response = requests.get(url)
-    with open('index.html', 'w', encoding='utf-8') as f:
-        f.write(response.text)
-"""
-def main():
-    get_html_and_save_to_file(COURSERA_URL + COURSES)
-"""
-
-
 
 def parse_specialization_page(url):
     """
@@ -59,11 +41,36 @@ def parse_course_page(url):
     Params:
     url - url of course page
     """
+    # dataset variables:
+    TITLE = 'TITLE'
+    URL = 'URL'
+    RATING = 'RATING'
+    NEW_CARRIER = 'NEW_CARRIER'
+    TAKE_ADVANTAGES = 'TAKE_ADVANTAGES'
+    EARN_MORE = 'EARN_MORE'
+    LEVEL = 'LEVEL'
+    LANGUAGE = 'LANGUAGE'
+    EXEC_TIME = 'EXEC_TIME'
+    CHAPTERS_CNT = 'CHAPTERS_CNT'
+    COURSE_DUR = 'COURSE_DUR'
+    CHAP_AVG_DUR = 'CHAP_AVG_DUR'
 
-    PARAM_1 = 'начал новую карьеру, пройдя эти курсы'
-    PARAM_2 = 'получил значимые преимущества в карьере благодаря этому курсу'
-    PARAM_3 = 'стал больше зарабатывать или получил повышение'
-    param_dict = {PARAM_1: VAR_1, PARAM_2: VAR_2, PARAM_3: VAR_3}
+    work = {
+        'начал новую карьеру, пройдя эти курсы': NEW_CARRIER,
+        'получил значимые преимущества в карьере благодаря этому курсу': TAKE_ADVANTAGES,
+        'стал больше зарабатывать или получил повышение': EARN_MORE
+    }
+
+    levels = {
+        'Начальный уровень': 1,
+        'Промежуточный уровень': 2,
+        'Продвинутый уровень': 3
+    }
+
+    languages = {
+        'Русский': 1,
+        'Английский': 2
+    }
 
     try:
         response = requests.get(COURSERA_URL + url)
@@ -73,10 +80,26 @@ def parse_course_page(url):
         print('Error: course page {} not found'.format(url))
         return None
 
-    data = {TITLE: '', RATING: None, VAR_1: None, VAR_2: None, VAR_3: None}
+    # data structure of sample
+    data = {
+        TITLE: '',
+        URL: COURSERA_URL + url,
+        RATING: None,
+        NEW_CARRIER: None,
+        TAKE_ADVANTAGES: None,
+        EARN_MORE: None,
+        LEVEL:None,
+        LANGUAGE: None,
+        EXEC_TIME: None,
+        CHAPTERS_CNT: None,
+        COURSE_DUR: None,
+        CHAP_AVG_DUR: None
+    }
     
+    # Collecting course title
     data.update({TITLE: soup.find('h1', {'class': 'banner-title m-b-0 banner-title-without--subtitle'}).next_element})
 
+    # Collecting course rating
     try:
         rating = 'empty-rating'
         rating = soup.find('span', {'class': '_16ni8zai m-b-0 rating-text number-rating number-rating-expertise'}).next_element
@@ -84,15 +107,68 @@ def parse_course_page(url):
     except:
         print('Error: cant convert value {} to float'.format(rating))
 
-    for tag in soup.find_all('div', class_='_1k3yl1y'):
+    # Collecting NEW_CARRIER, TAKE_ADVANTAGES, EARN_MORE variables
+    for tag in soup.find_all('div', {'class': '_1k3yl1y'}):
         param = tag.find('div', class_='font-sm').next_element
         value = tag.find('h2').next_element
         try:
-            if param in param_dict.keys():
-                data.update({param_dict.get(param): float(value)})
+            if param in work.keys():
+                data.update({work.get(param): float(value)})
         except:
             print('Error: cant convert value {} to float'.format(value))
 
+    # Collecting LEVEL, LANGUAGE, EXEC_TIME 
+    tags = soup.find('div', {'class': 'ProductGlance _9cam1z p-t-2'})
+    for tag in tags.find_all(class_=re.compile('_16ni8zai')):
+        element = tag.next_element
+        # If tag is about level
+        if element in levels.keys():
+            data.update({LEVEL: levels.get(element)})
+        # If tag is about language
+        if element in languages.keys():
+            data.update({LANGUAGE: languages.get(element)})
+        # If tag is about execution time:
+        try:
+            if re.match(r'Прибл.\s[0-9]+\s(ча(с|а|ов)|недел[иья])', element):
+                try:
+                    time = int(re.findall(r'[0-9]+', element))
+                    if element == re.compile(r'недел[иья]'):
+                        data.update({EXEC_TIME: 168*time})
+                    elif element == re.compile(r'ча(с|а|ов)'):
+                        data.update({EXEC_TIME: time})
+                    else:
+                        print('Unknown time')
+                except:
+                    print('Error: cant convert {} to int number from string'.format(element))
+        except:
+            pass
+        
+    # Chapters quantity is length of chapter description tag list
+    tags = soup.find_all(class_='_jyhj5r SyllabusWeek')
+    chapters_cnt = len(tags)
+    data.update({CHAPTERS_CNT: chapters_cnt})
+    # Collecting course and chapter estimated duration time 
+    course_dur = 0 # minutes
+    chap_avg_dur = 0 # minutes
+    for tag in tags:
+        element = tag.find('div', {'data-test': 'duration-text-section'}).find('span').next_element
+        print(element)
+        try:
+            time = int((re.findall(r'[0-9]+', element))[0])
+        except:
+            time = 0
+            print('Error: cant convert number to int from string {}'.format(element))
+        
+        if re.match(r'[0-9]+\sминут', element):
+            course_dur += time
+        elif re.match(r'[0-9]+\sч\.', element):
+            course_dur += 60*time
+
+    if chapters_cnt > 0:
+        chap_avg_dur = course_dur/chapters_cnt
+
+    data.update({COURSE_DUR: course_dur, CHAP_AVG_DUR: chap_avg_dur})
+    
     return data
 
 
@@ -103,7 +179,7 @@ def main():
     # list of courses sample
     sample = []
 
-    # get first page of course catalog
+    # Gettig first page of course catalog
     for i in range(1):
         # Gettig soup object of courses catalog page
         try:
@@ -119,16 +195,13 @@ def main():
             url = tag.find('a').get('href')
             card_info = tag.find(class_='card-info vertical-box')
             page_type = card_info.find(class_='_jen3vs _1d8rgfy3').next_element
-
-            data = {TITLE: '', RATING: None, VAR_1: None, VAR_2: None, VAR_3: None}
             
             if page_type == 'Курс':
                 # Calling course page parsing function by link in tag 'a':
                 # If parse_course_page is unseccessful it will return None
                 # If successful - data would updated with course vars and would inserted into sample
-                course_vars = parse_course_page(url)
-                if course_vars != None:
-                    data.update(course_vars)
+                data = parse_course_page(url)
+                if data != None:
                     sample.append(data)
 
             elif page_type == 'Специализация':
@@ -137,9 +210,8 @@ def main():
                 courses = parse_specialization_page(url)
                 if courses != None:
                     for course in courses:
-                        course_vars = parse_course_page(course)
-                        if course_vars != None: 
-                            data.update(course_vars)
+                        data = parse_course_page(course)
+                        if data != None:
                             sample.append(data)
 
             else:
@@ -153,4 +225,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
